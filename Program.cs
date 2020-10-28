1066e13ba55cc00e6d53c9b1f9c78b500f95eeb1
Inline2 <69176265+Inline2@users.noreply.github.com>
2020-10-28 16:08:02 -0400

    Second test
    Now if you have a different version of the file (different modification date) it will delete your file and replace it. I also changed the error to be more specific. Make sure you don't have any text files you don't have in your mega folder in your local folder as it will probably delete it.

--- Program.cs
+++ Program.cs
@@ -9,26 +9,53 @@ namespace mp3MegaSync
     {
         static void Main(string[] args)
         {
+            var fileNumber = 0;
             var client = new MegaApiClient();
 
             client.LoginAnonymous();
 
+            DirectoryInfo info = new DirectoryInfo(@".\");
+            FileInfo[] files = info.GetFiles("*.txt").OrderBy(p => p.Name).ToArray();
+            foreach (FileInfo file in files)
+            {
+                Console.WriteLine(file);
+            }
+
             Uri link = new Uri("https://mega.nz/folder/CsFjmCAD#a2uE2YVum3dV1lSSbPOXKg");
-            var nodes = client.GetNodesFromLink(link);
+
+            var nodes = client.GetNodesFromLink(link).OrderBy(s => s.Name);
 
             foreach (var node in nodes.Where(x => x.Type == NodeType.File))
             {
                 Console.WriteLine($"Downloading {node.Name}");
                 try
                 {
+                    Console.WriteLine(fileNumber);
+
+                    try
+                    {
+                        if (node.ModificationDate != files[fileNumber].LastWriteTime)
+                            File.Delete(files[fileNumber].Name);
+                    }
+                    catch(IndexOutOfRangeException){}
+
                     client.DownloadFile(node, node.Name);
-                    Console.WriteLine(node.Name);
+
+                    File.SetLastWriteTime(node.Name, node.ModificationDate.Value);
+                    //if (node.ModificationDate.ToString() != files[fileNumber].LastWriteTime.ToString())
+                    //{
+                    //    Console.WriteLine(node.Name);
+                    //    Console.WriteLine(node.ModificationDate);
+                    //    Console.WriteLine(files[fileNumber].Name);
+                    //    Console.WriteLine(files[fileNumber].LastWriteTime);
+                    //}
+                    //Temporary code block for debugging
                 }
                 catch (IOException)
                 {
-                    Console.WriteLine("System.IO.IOException: download failed!");
+                    Console.WriteLine("Download failed!\nSystem.IO.IOExcepction: you already have this file");
                 }
-                Console.WriteLine($"Finsished downloading {node.Name}");
+                fileNumber++;
             }
         }
     }
