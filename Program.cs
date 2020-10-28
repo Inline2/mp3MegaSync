159ac240ef5537cd56967b50298bd7b1f913f8e3
Inline2 <69176265+Inline2@users.noreply.github.com>
2020-10-28 13:16:09 -0400

    First testing of program
    It downloads all the files in the mega folder in the variable link and catches IOExceptions caused by downloading a file you already have

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using CG.Web.MegaApiClient;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace mp3MegaSync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new MegaApiClient();
+
+            client.LoginAnonymous();
+
+            Uri link = new Uri("https://mega.nz/folder/CsFjmCAD#a2uE2YVum3dV1lSSbPOXKg");
+            var nodes = client.GetNodesFromLink(link);
+
+            foreach (var node in nodes.Where(x => x.Type == NodeType.File))
+            {
+                Console.WriteLine($"Downloading {node.Name}");
+                try
+                {
+                    client.DownloadFile(node, node.Name);
+                    Console.WriteLine(node.Name);
+                }
+                catch (IOException)
+                {
+                    Console.WriteLine("System.IO.IOException: download failed!");
+                }
+                Console.WriteLine($"Finsished downloading {node.Name}");
+            }
+        }
+    }
+}
